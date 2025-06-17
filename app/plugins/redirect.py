f70f8d77d8074a3d8496df37415970564e147ed0
fraoustin <fraoustin@gmail.com>
2025-06-17 20:56:50 +0200

    <fix> preserve params in url

--- app/plugins/redirect.py
+++ app/plugins/redirect.py
@@ -1,13 +1,17 @@
-from fastapi.responses import RedirectResponse
 from fastapi import Request
-from typing import List
-
+from fastapi.responses import RedirectResponse
+from fastapi.routing import APIRouter
+from urllib.parse import urlencode
 
 def redirect(app, prefix_src="/api/latest", prefix_dst="/api/v1", methods=["GET", "POST", "PUT", "DELETE", "PATCH"]):
 
     @app.api_route(prefix_src + "/{path:path}", methods=methods, include_in_schema=False)
     async def redirect_to_prefix_dst(path: str, request: Request):
+        query_string = request.url.query
+        new_url = f"{prefix_dst}/{path}"
+        if query_string:
+            new_url += f"?{query_string}"
         return RedirectResponse(
-            url=f"{prefix_dst}/{path}",
-            status_code=307
+            url=new_url,
+            status_code=307  # Temporary redirect, preserves method and body
         )
